3f7bff1f87362944be41b9b048fadef869a4ab4b
Sean Joseph <theseanjoseph@gmail.com>
2021-12-04 12:05:43 -0500

    Working on the final part of section 1

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,34 @@
 # pcost.py
 #
 # Exercise 1.27
+
+import sys
+
+
+def portfolio_cost(filename):
+    with open(filename) as f:
+        keys = [key.strip() for key in next(f).split(",")]
+        rows = []
+        for row in f:
+            rows.append({key: col.strip() for key, col in zip(keys, row.split(","))})
+
+    total = 0
+
+    for row in rows:
+        symbol = row["name"]
+        try:
+            total += int(row["shares"]) * float(row["price"])
+        except ValueError:
+            print(f"Warning! Unable to parse shares or price for {symbol!r}")
+
+    return total
+
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = "Data/portfolio.csv"
+
+total_cost = portfolio_cost(filename)
+
+print(f"Total cost ${total_cost:0.2f}")
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,17 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+
+
+def read_portfolio(filename):
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        portfolio = []
+        for symbol, shares, price in rows:
+            try:
+                portfolio.append((symbol, int(shares), float(price)))
+            except ValueError:
+                print("Warning! Failed to parse shares or price for symbol", symbol)
